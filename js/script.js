22e0d880e54a4e562e4172f8b04164bf3df78e5e
kalyanimurkute <murkutekalyani259@gmail.com>
2025-08-17 13:53:56 +0530

    Update script.js
    update file

--- js/script.js
+++ js/script.js
@@ -6,21 +6,21 @@ const image = document.getElementById('dice');
             const Number = Math.floor(Math.random() * (max - min) + min);
 
             if (Number == 1) {
-                image.src = "../icon/dice (3).png";
+                image.src = "../icons/dice (3).png";
             }
             if (Number == 2) {
-                image.src = "../icon/dice (6).png";
+                image.src = "../icons/dice (6).png";
             }
             if (Number == 3) {
-                image.src = "../icon/dice (5).png";
+                image.src = "../icons/dice (5).png";
             }
             if (Number == 4) {
-                image.src = "../icon/dice (5).png";
+                image.src = "../icons/dice (5).png";
             }
             if (Number == 5) {
-                image.src = "../icon/dice (7).png";
+                image.src = "../icons/dice (7).png";
             }
             if (Number == 6) {
-                image.src = "../icon/dice (2).png";
+                image.src = "../icons/dice (2).png";
             }
         }
